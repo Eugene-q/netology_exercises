7b614b1f90c14d33c54c0a5aa896d609c87a06d8
Eugene511Git <eaejob@mail.ru>
2017-06-04 02:17:54 +0400

    add Counter

--- 2_3/news_mining.py
+++ 2_3/news_mining.py
@@ -1,41 +1,30 @@
 import json
 import chardet
+from collections import Counter
 
 def main():
   files = ('newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json')
   for file in files:
     news = load(file)
     print(news['rss']['channel']['title'], '\n')
-    words = get_words(news)
-    words_ratings = to_rate(words)
-    for i, rating in zip(range(10), reversed(sorted(words_ratings))):
-      print('{:>15} {}'.format(words_ratings[rating], rating))
+    words = Counter(get_words(news))
+    for word, rating in words.most_common(10):
+      print('{:>15} {}'.format(word, rating))
     print()
     
 
-def to_rate(words):
-  rated_words = list()
-  ratings = list()
-  for word in words:
-    if word not in rated_words:
-      rated_words.append(word)
-      ratings.append(words.count(word))
-  return dict(zip(ratings, rated_words))
-    
-    
 def get_words(news):
   all_text = ''
   for item in news['rss']['channel']['item']:
       if isinstance(item['title'], str): all_text += item['title'] + ' ' + item['description']
       else: all_text += item['title']['__cdata'] + ' ' + item['description']['__cdata']
-  return list(filter(lambda x: len(x) > 6, list(map(lambda x: x.strip(',.<br>'), all_text.split()))))
+  return filter(lambda x: 6 < len(x) < 25, map(lambda x: x.strip(',.<br>/'), all_text.split()))
     
     
 def load(file):
   with open(file, 'rb') as f:
     bytes = f.read()
     return json.loads(bytes.decode(chardet.detect(bytes)['encoding']))
-    #print(news_block['rss']['channel']['title'])
     
     
 main()
