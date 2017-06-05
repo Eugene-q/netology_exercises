65cd1addd8e5c4fdb8b9d72f903165883dd64520
Eugene511Git <eaejob@mail.ru>
2017-06-05 11:57:46 +0400

    use multiprocessing

--- 2_5/image_resize.py
+++ 2_5/image_resize.py
@@ -1,11 +1,19 @@
 import os
 import glob
 import subprocess
+from multiprocessing import Pool
+
 
 def main():
-  converter_path = os.path.join('C:\\', 'Program Files', 'ImageMagick-7.0.5-Q16', 'magick.exe')
-  for source_image in glob.glob('Source/*.jpg'):
+    if __name__ == '__main__':
+        with Pool(4) as p:
+            p.map(resize, glob.glob('Source/*.jpg'))
+
+
+def resize(source_image):
+    converter_path = os.path.join('C:\\', 'Program Files', 'ImageMagick-7.0.5-Q16', 'magick.exe')
     out_image = os.path.join('Result', os.path.split(source_image)[1])
     subprocess.run('{} convert {} -resize 200 {}'.format(converter_path, source_image, out_image))
+
 
 main()
