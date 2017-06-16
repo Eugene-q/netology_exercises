a8b787d27a79d242e2f18625e1a1951ef0077b71
Eugene511Git <eaejob@mail.ru>
2017-06-16 23:22:22 +0300

    fix convert.exe usage

--- 2_5/image_resize.py
+++ 2_5/image_resize.py
@@ -5,15 +5,17 @@ from multiprocessing import Pool
 
 
 def main():
+    out_dir = os.path.join(os.getcwd(), 'Result')
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
     if __name__ == '__main__':
-        with Pool(4) as p:
+        with Pool() as p:
             p.map(resize, glob.glob('Source/*.jpg'))
 
 
 def resize(source_image):
-    converter_path = os.path.join('C:\\', 'Program Files', 'ImageMagick-7.0.5-Q16', 'magick.exe')
     out_image = os.path.join('Result', os.path.split(source_image)[1])
-    subprocess.run('{} convert {} -resize 200 {}'.format(converter_path, source_image, out_image))
+    subprocess.run('convert.exe {} -resize 200 {}'.format(source_image, out_image))
 
 
 main()
