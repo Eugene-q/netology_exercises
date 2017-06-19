64f85e353214f8f9bd3b58c5bb0078e7ec21c2ce
Eugene511Git <eaejob@mail.ru>
2017-06-19 11:13:03 +0300

    delete service code

--- 3_4/VKboy.py
+++ 3_4/VKboy.py
@@ -1,6 +1,4 @@
 import requests
-from pprint import pprint
-import random
 
 VERSION = '5.65'
 VK_METHOD = 'https://api.vk.com/method/'
@@ -13,7 +11,6 @@ def friends_of(id, output='list'):
         'v': VERSION,
     }
     response = requests.get(''.join((VK_METHOD, 'friends.get')), params)
-    # print(response.json())
     if response.json().get('error'):
         return []
     elif output == 'count':
@@ -23,22 +20,15 @@ def friends_of(id, output='list'):
 
 def common_friends(user):
     user_friends_ids = friends_of(user)
-    friends_of_all_ids = set(friends_of(user_friends_ids[0]))
+    common_friends_ids = set(friends_of(user_friends_ids[0]))
     for friend_id in user_friends_ids:
-        # print('friend id : ', friend_id)
-        # print(get_users(friend_id))
-        # print('all : ', friends_of_all_ids)
-        # print('friends of friend : ', friends_of(friend_id))
         if get_users(friend_id)[0].get('deactivated'):
             continue
-        intersected = set(friends_of(friend_id)).intersection(friends_of_all_ids)
+        intersected = set(friends_of(friend_id)).intersection(common_friends_ids)
         if not intersected:
             break
-        # print('intercec : ', intersected)
-        friends_of_all_ids = intersected
-        # print('all : ', friends_of_all_ids)
-        # print()
-    return friends_of_all_ids
+        common_friends_ids = intersected
+    return common_friends_ids
 
 
 def get_users(user_ids, name_case='nom'):
@@ -52,17 +42,12 @@ def get_users(user_ids, name_case='nom'):
 def main():
     user = get_users(USER_ID, 'gen')[0]
     user_name = '{} {}'.format(user['first_name'], user['last_name'])
-
     print('Количество друзей {} - {}.'.format(user_name, friends_of(user['uid'], 'count')))
     print('Общие друзья всех его друзей:')
-    user_common_friends_ids = common_friends(user['uid'])
-    if not user_common_friends_ids:
-        print('отсутствуют')
-    else:
-        friends = get_users(user_common_friends_ids)
-        for friend in friends:
-            print('{} {}'.format(friend['first_name'], friend['last_name']))
+    common_friends_of_friends_ids = common_friends(user['uid'])
+    common_friends_of_friends = get_users(common_friends_of_friends_ids)
+    for friend in common_friends_of_friends:
+        print('{} {}'.format(friend['first_name'], friend['last_name']))
 
 
 main()
-
