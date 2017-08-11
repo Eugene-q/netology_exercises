0f28dee6e3635a3b123638e8a5152de70b3b0547
Eugene511Git <eaejob@mail.ru>
2017-08-11 09:11:21 +0300

    add universal token

--- diplom/spy_game.py
+++ diplom/spy_game.py
@@ -30,7 +30,7 @@ VK_METHOD = 'https://api.vk.com/method/'
 # print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
 
 
-TOKEN = '410f000f45e6aba9dc6e11fadbebe30ca9910ab2505c1dabb4311671475efe6dc5b131651ab7a78174cc8'
+TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
 
 
 class Show:
